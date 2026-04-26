c00763cd38bd8f9ecbffa9e54ce3e4b08961ab31
NormundsBrizs <normundsbrizs@gmail.com>
2026-04-26 11:14:44 +0300

    Add: added steps up to image upload

--- cypress/e2e/final_task.cy.js
+++ cypress/e2e/final_task.cy.js
@@ -7,7 +7,35 @@ describe('DEMO QA', () => {
     });
 
     it.only('Input information and validate', () => {
-      // 
+      // Define test data
+      const testData = {
+        name: 'John',
+        surname: 'Doe',
+        email: 'johndoe@test.com',
+        mobileNumber: '1234567890',
+        subject: 'Economics',
+      }
+      // Input First Name, Last Name, Email, Gender and Mobile Number with test data
+      PracticeFormPage.nameField.type(testData.name);
+      PracticeFormPage.surnameField.type(testData.surname);
+      PracticeFormPage.emailField.type(testData.email);
+      PracticeFormPage.genderRadioMale.click();
+      PracticeFormPage.phoneField.type(testData.mobileNumber);
+      // Set Date of Birth to 28th of February, 1930
+      PracticeFormPage.dateOfBirthField.click();
+      PracticeFormPage.dateOfBirthMonth.select('February');
+      PracticeFormPage.dateOfBirthYear.select('1930');
+      PracticeFormPage.dateOfBirthDay28.click();
+      // Set Subjects to Economics
+      PracticeFormPage.subjectsField.type(testData.subject);
+      PracticeFormPage.subjectSelect(testData.subject);
+      // Set Hobbies to Music
+      PracticeFormPage.hobbiesRadioMusic.click();
+      // Upload an image of your choice
+      // Set State to NCR
+      // Set City to Delhi
+      // Click Submit
+      // Validate that each Labeled row contains the correct information
     })
   })
 })
--- cypress/pageObjects/practiceFormPage.js
+++ cypress/pageObjects/practiceFormPage.js
@@ -4,4 +4,52 @@ export class PracticeFormPage extends BasePage {
     static get url() {
         return '';
     }
+
+    static get nameField() {
+        return cy.get('#firstName');
+    }
+
+    static get surnameField() {
+        return cy.get('#lastName');
+    }
+
+    static get emailField() {
+        return cy.get('#userEmail');
+    }
+
+    static get genderRadioMale() {
+        return cy.get('#gender-radio-1');
+    }
+
+    static get phoneField() {
+        return cy.get('#userNumber');
+    }
+
+    static get dateOfBirthField() {
+        return cy.get('#dateOfBirthInput');
+    }
+
+    static get dateOfBirthMonth() {
+        return cy.get('.react-datepicker__month-select');
+    }
+
+    static get dateOfBirthYear() {
+        return cy.get('.react-datepicker__year-select');
+    }
+
+    static get dateOfBirthDay28() {
+        return cy.get('.react-datepicker__day--028:not(.react-datepicker__day--outside-month)');
+    }
+
+    static get subjectsField() {
+        return cy.get('.subjects-auto-complete__input-container');
+    }
+
+    static subjectSelect(subject) {
+        cy.contains('.subjects-auto-complete__option', subject).click();
+    }
+
+    static get hobbiesRadioMusic() {
+        return cy.get('#hobbies-checkbox-3');
+    }
 }
